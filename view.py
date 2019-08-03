4d19a7998e45c9ba4befdcf8f41714a95ac57b03
yogesh21saggit <52128720+yogesh21saggit@users.noreply.github.com>
2019-08-03 22:45:33 +0530

    Update view.py

--- view.py
+++ view.py
@@ -10,20 +10,16 @@ def removepunc(request):
 
     import requests
     res = requests.get('http://localhost:9200')
-    #print(res.content)
+   
     from elasticsearch import Elasticsearch
     es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-    #print(es.get(index='test-index', doc_type='test', id=1))
-   #print(es.search(index="product", body={"query": {"match": {"title": djtext}}}))
-#d =(es.search(index="product", body={"query": {"match": {"title": djtext}}}))
+    
     dd = es.search(index="product", body={"query": {"match":
 
 
     {"title": djtext}},"highlight":{"fields":{"title":{}}
 
-        #{"pre_tags":"<marks>","post_tags":"</marks>"{"fields":{"title":{}}}
-                                    }})
-    #de = json.dumps(dd)
+     
     comp_data = []
     comp_doc = {}
     for doc in dd['hits']['hits']:
@@ -37,26 +33,7 @@ def removepunc(request):
                     comp_data.append(comp_doc)
                 except:
                     pass
-        #print( doc["_source.title"])
-    #print(dd)
-    #print("%d documents found" % dd['hits']['total'])
-    #for doc in dd['hits']['hits']:
-        #print("%s) %s" % (doc['_id'], doc['_source']['content']))
-
-    #for p in dd['title']:
-        #print('title: ' + p['title'])
-    #3for hit in  dd['hits']['hits']:
-        #dj = dd['hits']['hits']
-        #de = hit['_source']['title']
-        #de = de + de
-
-        #df = hit ['_source'] ['description']
-        #df = df + "/n" + df
-        #print(dj)
-    #comp_data.append(comp_doc)
+       
     parms = {'search_result': comp_data}
     return render(request, 'out.html',parms)
-    #return render("testing")
-    #return HttpResponse("remove punc")
-#print(request.GET.get('text','default)
-#return HttpResponse ("REMOVE PUNCS")
+    
